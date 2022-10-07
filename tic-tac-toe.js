52f71d72fc11b0410d051725afad173f759bbb67
Kareem Ellis <kareemellis55@gmail.com>
2022-10-07 00:13:53 -0500

    Added functionality to place X/O on squares

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,13 +1,44 @@
 window.addEventListener('load', function () {
     alert("Page Loaded")
     const board = document.getElementById("board")
-    console.log(board)
     const squares = Array.from(board.getElementsByTagName("div"))
-    console.log(squares)
+
+    //Set starting player
+    let currentPlay = "0";
+    //Switches the players
+    let switchPlayer = function(){
+        console.log("Player has placed " + currentPlay)
+        if (currentPlay == "0") {
+            currentPlay = "X"
+        }else{
+            currentPlay = "0"
+        }
+    }
+
+    //Set array to store record of game
+    let plays = [" ", " ", " ",
+                 " ", " ", " ",
+                 " ", " ", " "]
+
     squares.forEach(square => {
         square.classList.add("square")
+        
+        //Functionality for clicking a square
+        square.addEventListener("click", () => {
+            if (square.innerText == "") {
+                square.innerText = currentPlay
+                
+                //Adds move to the plays array to record game
+                plays[squares.indexOf(square)] = currentPlay
+                console.log("Game Recor:\n" + plays)
+
+                switchPlayer()
+            }
+            
+        })
     });
     
+
 })
 
 
